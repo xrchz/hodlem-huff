6e5f1fb12f55534a869e563d605e55e79df08802
Ramana Kumar <ramana@member.fsf.org>
2023-03-11 02:16:57 +0000

    feat: implement newDeck

--- test/SimpleStore.t.sol
+++ test/SimpleStore.t.sol
@@ -8,10 +8,12 @@ import "forge-std/console.sol";
 contract SimpleStoreTest is Test {
     /// @dev Address of the SimpleStore contract.
     SimpleStore public simpleStore;
+    Deck public deck;
 
     /// @dev Setup the testing environment.
     function setUp() public {
         simpleStore = SimpleStore(HuffDeployer.deploy("SimpleStore"));
+        deck = Deck(HuffDeployer.deploy("Deck"));
     }
 
     /// @dev Ensure that you can set and get the value.
@@ -21,9 +23,31 @@ contract SimpleStoreTest is Test {
         console.log(simpleStore.getValue());
         assertEq(value, simpleStore.getValue());
     }
+
+    function testNewDeck() public {
+      deck.newDeck(12, 13);
+    }
+
+    function testNewDeckInvalid() public {
+      vm.expectRevert(abi.encodeWithSelector(Deck.InvalidArgument.selector));
+      deck.newDeck(0, 13);
+    }
+
+    function testChangeAddress() public {
+      vm.expectRevert();
+      deck.changeAddress(1, 1, address(this));
+    }
 }
 
 interface SimpleStore {
     function setValue(uint256) external;
     function getValue() external returns (uint256);
+}
+
+interface Deck {
+	error InvalidArgument();
+	error Unauthorised();
+	function changeAddress(uint256, uint256, address) external;
+	function changeDealer(uint256, address) external;
+	function newDeck(uint256, uint256) external;
 }
